8063b8633387ab4871b26b21bfb8f62728d0b833
Shreyansh Jain <suranashreyansh019@gmail.com>
2026-05-20 21:55:21 +0530

    Enable public and private access for EKS endpoint

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -7,6 +7,9 @@ module "eks" {
 
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
+  endpoint_public_access = true
+  endpoint_private_access = true
+  
 
   enable_cluster_creator_admin_permissions = true
 
